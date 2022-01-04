e6cc11ca563cbca06136dc895b6005d3b114f244
Christina <christinaraganit@gmail.com>
2022-01-03 23:00:52 -0800

    restyling updates :stars: :3

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,21 @@ import "../style/Header.css";
 import HeaderImage from "../img/IMG_1702.png";
 
 function TitleHeader() {
-    return (
-        <div className="TitleHeader">
-            <div className="TextColumn">
-                <span className="Title">Hi my name is Chris Raganit owo</span>
-                <span className="Subtitle">owo hi my name is chris raganit and i like eric :3</span>
-            </div>
-            <div className="ImageColumn">
-                <img src={HeaderImage} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="TitleHeader">
+      <div className="TextColumn">
+        <p className="IntroTitle">HI, MY NAME IS</p>
+        <span className="Title">Christina Raganit</span>
+        <span className="Subtitle">
+          I am a UI/UX designer with a diploma in Computer Systems and a current
+          SFU student studying Interactive Arts and Technology.
+        </span>
+      </div>
+      <div className="ImageColumn">
+        <img src={HeaderImage} />
+      </div>
+    </div>
+  );
 }
 
 export default TitleHeader;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,32 @@
 import "../style/Navigation.css";
-
+import StarLinkImg from "../img/star.svg";
 
 function Navigation() {
-    return(
-        <div className="NavigationMenu">
-            <a href="#">Writing</a>
-            <a href="#">About</a>
-            <a href="#">Experience</a>
-            <a href="#">Projects</a>
-            <a href="#">Contact</a>
-            <a href="#">Resume</a>
-            <a href="#">☀️</a>
-        </div>
-    );
+  return (
+    <div className="NavigationMenu">
+      <a className="NavigationLink" href="#">
+        Home
+      </a>
+      <a className="NavigationLink" href="#">
+        About
+      </a>
+      <a className="NavigationLink" href="#">
+        Experience
+      </a>
+      <a className="NavigationLink" href="#">
+        Projects
+      </a>
+      <a className="NavigationLink" href="#">
+        Resume
+      </a>
+      <div className="PrimaryNavigationLink">
+        <a className="NavigationLink" href="#">
+          Contact
+        </a>
+        <img src={StarLinkImg} />
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
