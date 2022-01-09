187dc77ba4319f5419ffc0afb6143b383c4938de
Christina <christinaraganit@gmail.com>
2022-01-08 22:02:35 -0800

    Timeline done :3

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,25 @@
 import StarLinkImg from "../img/star.svg";
+import PointedStarLinkImg from "../img/pointer-star.svg";
 import "../style/Experience.css";
 
-function ExperienceDiv(props) {
+function TimelineItem(props) {
   return (
-    <div className={"ExperienceDiv ExperienceDiv" + props.Position}>
-      <p className="ExperienceTitle">{props.ExperienceTitle}</p>
-      <p className="ExperienceLocation">{props.ExperienceLocation}</p>
-      <ul className="ExperienceDescription">
-        {props.ExperienceDescription.map((element) => {
-          return <li>{element}</li>;
-        })}
-      </ul>
+    <div className={"TimelineItem"}>
+      <div className="TimelineIcon">
+        <img src={PointedStarLinkImg}></img>
+      </div>
+      <div className={"TimelineContent " + props.Position + " " + props.Last}>
+        <h2 className="ExperienceTitle">{props.ExperienceTitle}</h2>
+        <p className="ExperienceLocation">{props.ExperienceLocation}</p>
+        <ul className="ExperienceDescription">
+          {props.ExperienceDescription.map((element) => {
+            return <li>{element}</li>;
+          })}
+        </ul>
+        <a href="#" class="ExperienceButton">
+          {props.ExperienceButton}
+        </a>
+      </div>
     </div>
   );
 }
@@ -23,13 +32,113 @@ function Timeline() {
         Experience
       </div>
 
-      <div class="Timeline">
-        <ExperienceDiv
-          Position="Right"
-          ExperienceTitle="Social Media Content Coordinator"
-          ExperienceLocation="Simon Fraser University Science Fiction and Fantasy Book Club | Burnaby, BC"
-          ExperienceDescription={["Lorem", "ipsum", "dolor", "sit", "amet"]}
-        ></ExperienceDiv>
+      <div class="Container">
+        <div id="Timeline">
+          <TimelineItem
+            Last="NotLast"
+            Position="Left"
+            ExperienceTitle="Social Media Content Coordinator"
+            ExperienceLocation="Simon Fraser University Science Fiction and Fantasy Book Club | Burnaby, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="NotLast"
+            Position="Right"
+            ExperienceTitle="UI/UX Developer"
+            ExperienceLocation="Karma Well Health Technologies | Greater Vancouver, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="NotLast"
+            Position="Left"
+            ExperienceTitle="Social Media Content Contributor"
+            ExperienceLocation="British Columbia Institute of Technology | Burnaby, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="NotLast"
+            Position="Right"
+            ExperienceTitle="Development Support"
+            ExperienceLocation="Keywords Studios | Burnaby, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="NotLast"
+            Position="Left"
+            ExperienceTitle="Workshop Facilitator"
+            ExperienceLocation="British Columbia Institute of Technology | Burnaby, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="NotLast"
+            Position="Right"
+            ExperienceTitle="Media and Public Relations Crew Member"
+            ExperienceLocation="Salmon Habitat Restoration Program | Surrey, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+
+          <TimelineItem
+            Last="Last"
+            Position="Left"
+            ExperienceTitle="Graphic Design Intern"
+            ExperienceLocation="Copytek Print and Sign | Surrey, British Columbia"
+            ExperienceDescription={[
+              "Lorem",
+              "ipsum",
+              "dolor",
+              "sit",
+              "amet :3",
+            ]}
+            ExperienceButton="View code on GitHub"
+          ></TimelineItem>
+        </div>
       </div>
     </div>
   );
