da84280ccf9990d862ab1f382c1f3b30e304e001
Christina <christinaraganit@gmail.com>
2022-01-29 23:59:57 -0800

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -48,7 +48,7 @@ function Navigation() {
             <Link to="/projects" onClick={closeMenu}>
               <span>Projects</span>
             </Link>
-            <a href="https://drive.google.com/file/d/1sslvr0mTPN6C2zCdPdID72X5CEz3WLkb/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1u-gDM-HkO_jH4ls1bodWWji_FmmGQtJk/view?usp=sharing">
               Resume
             </a>
           </div>
